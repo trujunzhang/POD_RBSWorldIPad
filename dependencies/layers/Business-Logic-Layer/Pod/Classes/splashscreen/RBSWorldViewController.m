cd222d18203d9630544a80bbca738b7b0105bbaf
wanghaogithub720 <wanghaomspace@gmail.com>
2015-01-15 21:50:21 +0800

    dev splashscreen.

--- dependencies/layers/Business-Logic-Layer/Pod/Classes/splashscreen/RBSWorldViewController.m
+++ dependencies/layers/Business-Logic-Layer/Pod/Classes/splashscreen/RBSWorldViewController.m
@@ -14,4 +14,18 @@
 @implementation RBSWorldViewController {
 
 }
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+
+    UIInterfaceOrientation toInterfaceOrientation = [UIApplication sharedApplication].statusBarOrientation;
+    BOOL isPortrait = (toInterfaceOrientation == UIInterfaceOrientationPortrait) || (toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown);
+
+    if(isPortrait) {
+        splashscreenBgViewer.image = [UIImage imageNamed:@"Default-Portrait.png"];
+    } else {
+        splashscreenBgViewer.image = [UIImage imageNamed:@"Default-Landscape.png"];
+    }
+}
+
 @end
